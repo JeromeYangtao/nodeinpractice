a1c1c8a3643553ee3a9d99e36ef8bffc25597a84
Thomson <779387850@qq.com>
2018-05-22 19:03:07 +0800

    update

--- src/2/arch.2.13.js
+++ src/2/arch.2.13.js
@@ -1,10 +1,10 @@
-switch(process.arch) {
-	case 'x64':
-		require('./lib.x64.node');
-		break;
-	case 'ia32':
-		require('./lib.Win32.node');
-		break;
-	default:
-		throw new Error('Unsupported process arch:', porcess.arch);
+switch (process.arch) {
+  case 'x64':
+    require('./lib.x64.node')
+    break
+  case 'ia32':
+    require('./lib.Win32.node')
+    break
+  default:
+    throw new Error('Unsupported process arch:', porcess.arch)
 }
